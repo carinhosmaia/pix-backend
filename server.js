8f58f9926ebd23cb700932a87bf133410eca4515
carinhosmaia <carinhosmaia924@gmail.com>
2025-12-14 21:35:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 
@@ -63,5 +65,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000);
+
 
 
